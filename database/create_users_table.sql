38f7e15cb59aac1b340853996e9659f30a161c45
ynhx <yinnndlovu@gmail.com>
2025-04-07 07:51:42 +0200

    Purge back-end files from front end

--- database/create_users_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    role ENUM('HR', 'Supervisor', 'Student') NOT NULL,
-    student_number VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
